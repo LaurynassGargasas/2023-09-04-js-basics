ee3dfeef05ea4a886451f45630a81bdd9df277f6
LaurynassGargasas <laurynas.gargasas@gmail.com>
2023-09-05 08:26:32 +0300

    update

--- task.js
+++ task.js
@@ -1,13 +1,13 @@
 let password = 'manoslaptazodis'
 
-if (password.length < 16) {
+if (!password.includes('#')) {
+    console.log('Slaptažodis privalo turėti grotažymes')
+} else if (password.length < 16) {
     console.log('Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo.')
 } else if (password.length > 15 && password.length > 20) {
     console.log('Slaptažodis tinkamas')
 } else if (password.length > 15 && password.length < 20){
     console.log('Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo.')
-} if (!password.includes('#')) {
-    console.log('Slaptažodis privalo turėti grotažymes')
 }
 
 
